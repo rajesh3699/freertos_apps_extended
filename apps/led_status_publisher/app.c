6bc8964b5b4c771e6eb93f5df0980b23b9ea16de
Rajesh Nagula <rajeshn0811@gmail.com>
2023-07-05 13:35:14 -0400

    Update app.c

--- apps/led_status_publisher/app.c
+++ apps/led_status_publisher/app.c
@@ -68,10 +68,9 @@ void appMain(void * arg)
 		 
 
 		rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100));
-		 if(HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13))
-	  		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, GPIO_PIN_SET);
-	  	else
-	  		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, GPIO_PIN_RESET);
+		
+	  	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, GPIO_PIN_SET);
+	  	
 		usleep(100000);
 	}
 
