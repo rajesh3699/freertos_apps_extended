ef2b4d75c2b4b7228b4c673fd0c585a51a6dea35
Rajesh Nagula <rajeshn0811@gmail.com>
2023-07-05 15:55:42 -0400

    Update main.c

--- microros_nucleo_f767zi_extensions/Src/main.c
+++ microros_nucleo_f767zi_extensions/Src/main.c
@@ -371,7 +371,7 @@ static void MX_GPIO_Init(void)
 
   /*Configure GPIO pin : USER_Btn_Pin */
   GPIO_InitStruct.Pin = USER_Btn_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(USER_Btn_GPIO_Port, &GPIO_InitStruct);
 
